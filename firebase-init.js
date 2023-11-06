60d5c5a84254e3c7be5d36804355c1600dd46630
Mauricio Teixeira <mauriciolt@gmail.com>
2023-11-06 01:27:34 -0300

    Aspectos de segurança do login.

---
+++ firebase-init.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBo_UiE0gvszQ3nFWY9XsiPCjq9_Da4xLk",
+    authDomain: "contabil-19553.firebaseapp.com",
+    projectId: "contabil-19553",
+    storageBucket: "contabil-19553.appspot.com",
+    messagingSenderId: "267023208204",
+    appId: "1:267023208204:web:80c23a126c8fbf5fc00329"
+    };
+    firebase.initializeApp(firebaseConfig);
